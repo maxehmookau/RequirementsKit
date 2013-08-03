69a7ae17ef0fa9816b81ab1c7341efdd6b1bee96
Max Woolf <maximus@zingysaturn.co.uk>
2013-08-03 15:40:00 +0100

    add projects stubs

---
+++ RequirementsKit/RKProject.h
@@ -0,0 +1,13 @@
+//
+//  RKProject.h
+//  RequirementsKit
+//
+//  Created by Max Woolf on 03/08/2013.
+//  Copyright (c) 2013 GatherRequirements. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RKProject : NSObject
+
+@end
---
+++ RequirementsKit/RKProject.m
@@ -0,0 +1,13 @@
+//
+//  RKProject.m
+//  RequirementsKit
+//
+//  Created by Max Woolf on 03/08/2013.
+//  Copyright (c) 2013 GatherRequirements. All rights reserved.
+//
+
+#import "RKProject.h"
+
+@implementation RKProject
+
+@end
